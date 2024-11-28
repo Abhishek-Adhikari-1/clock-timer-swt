e07fe569590aead0e511f849fbf0fa3bbaba9590
Abhishek-Adhikari-1 <adhikaryabhishek209@gmail.com>
2024-11-28 07:49:30 +0545

    new

--- js/script.js
+++ js/script.js
@@ -1,74 +1,89 @@
-let hr = min = sec = ms = "0" + 0,
-    startTimer;
+let hr = (min = sec = ms = "0" + 0),
+  startTimer;
 
 var startBtn = document.querySelector(".buttons .start"),
-    stopBtn = document.querySelector(".buttons .stop"),
-    resetBtn = document.querySelector(".buttons .reset");
+  stopBtn = document.querySelector(".buttons .stop"),
+  resetBtn = document.querySelector(".buttons .reset");
 
 startBtn.addEventListener("click", () => {
-    StartBtn();
+  StartBtn();
 });
 
 resetBtn.addEventListener("click", () => {
-    Reset();
+  Reset();
 });
 
 stopBtn.addEventListener("click", () => {
-    Stop();
+  Stop();
 });
 
 const StartBtn = () => {
-    startBtn.classList.add("active");
-    startBtn.disabled = true;
+  startBtn.classList.add("active");
+  startBtn.disabled = true;
 
-    startTimer = setInterval(() => {
-        ms++;
-        ms = ms < 10 ? "0" + ms : ms;
-        if (ms == 100) {
-            sec++;
-            sec = sec < 10 ? "0" + sec : sec;
-            ms = "0" + 0;
-            // /sadasdasd
-        }
-        if (sec == 60) {
-            min++;
-            min = min < 10 ? "0" + min : min;
-            sec = "0" + 0;
-        }
-        if (min == 60) {
-            hr++;
-            hr = hr < 10 ? "0" + hr : hr;
-            min = "0" + 0;
-        }
-        putValue();
-    }, 10);
+  startTimer = setInterval(() => {
+    ms++;
+    ms = ms < 10 ? "0" + ms : ms;
+    if (ms == 100) {
+      sec++;
+      sec = sec < 10 ? "0" + sec : sec;
+      ms = "0" + 0;
+      // /sadasdasd
+    }
+    if (sec == 60) {
+      min++;
+      min = min < 10 ? "0" + min : min;
+      sec = "0" + 0;
+    }
+    if (min == 60) {
+      hr++;
+      hr = hr < 10 ? "0" + hr : hr;
+      min = "0" + 0;
+    }
+    putValue();
+  }, 10);
 };
 
 const putValue = () => {
-    document.querySelector(".time .millisecond").innerText = ms;
-    document.querySelector(".time .second").innerText = sec;
-    document.querySelector(".time .minute").innerText = min;
-    document.querySelector(".time .hour").innerText = hr;
+  document.querySelector(".time .millisecond").innerText = ms;
+  document.querySelector(".time .second").innerText = sec;
+  document.querySelector(".time .minute").innerText = min;
+  document.querySelector(".time .hour").innerText = hr;
 };
 
 const Reset = () => {
-    Stop();
-    ms = "00";
-    sec = "00";
-    min = "00";
-    hr = "00";
-    putValue();
-    startBtn.disabled = false;
+  Stop();
+  ms = "00";
+  sec = "00";
+  min = "00";
+  hr = "00";
+  putValue();
+  startBtn.disabled = false;
 };
 
 const Stop = () => {
-    startBtn.classList.remove("active");
-    clearInterval(startTimer);
-    startBtn.disabled = false;
+  startBtn.classList.remove("active");
+  clearInterval(startTimer);
+  startBtn.disabled = false;
 };
 
 const addActive = () => {
-    startBtn.classList.remove("active");
-    stopBtn.classList.add("active");
-    resetBtn.classList.remove("active");
+  startBtn.classList.remove("active");
+  stopBtn.classList.add("active");
+  resetBtn.classList.remove("active");
 };
+
+// this is new comment
+// this is new comment
+// this is new comment
+// this is new comment
+// this is new comment
+// this is new comment
+// this is new comment
+// this is new comment
+// this is new comment
+// this is new comment
+// this is new comment
+// this is new comment
+// this is new comment
+// this is new comment
